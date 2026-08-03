5cd482208bafc0730be1da9c5341565301e076fc
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-08-03 13:38:57 +0200

    REFACTOR: Removed a unneccesary method and added an Excalidraw image

--- Kartlegging.Test/KartleggingsTest.cs
+++ Kartlegging.Test/KartleggingsTest.cs
@@ -5,15 +5,7 @@ namespace Kartlegging.Test;
 public class UnitTest1
 {
     [Fact]
-    public void LvlOne()
-    {
-        var lvl = new LvL();
-        var addXp = lvl.AddOneXp();
-        Assert.Equal(1, addXp);
-    }
-
-    [Fact]
-    public void LvlTwo()
+    public void AddXpTest()
     {
         var lvl = new LvL();
         var addLvl = lvl.AddXp(10);
--- Kartlegging/Kartlegging.cs
+++ Kartlegging/Kartlegging.cs
@@ -7,8 +7,6 @@ public class LvL
 {
     int level = 1;
 
-    public int AddOneXp()   => 1;
-
     public int AddXp(int a) => a >= 10 ? level + 1 : level;
  
 }
