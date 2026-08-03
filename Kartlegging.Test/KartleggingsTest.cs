557da6bb96b1ad678c282aa1d9922d5b7662e1ff
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-08-03 13:45:06 +0200

    REFACTOR: Added some extra steps in the test to check all level two cases

--- Kartlegging.Test/KartleggingsTest.cs
+++ Kartlegging.Test/KartleggingsTest.cs
@@ -8,7 +8,12 @@ public class UnitTest1
     public void AddXpTest()
     {
         var lvl = new LvL();
-        var addLvl = lvl.AddXp(10);
-        Assert.Equal(2, addLvl);
+        var addNoLvl    = lvl.AddXp(5);
+        var addLvl      = lvl.AddXp(10);
+        var addLvlAndXp = lvl.AddXp(15);
+
+        Assert.NotEqual (2, addNoLvl);
+        Assert.Equal    (2, addLvl);
+        Assert.Equal    (2, addLvlAndXp);
     }    
 }
