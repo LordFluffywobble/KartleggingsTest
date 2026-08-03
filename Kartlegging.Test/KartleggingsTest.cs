f710daeeae8aeb20995746ebb72ef951f275cffb
LordFluffywobble <38191273+LordFluffywobble@users.noreply.github.com>
2026-08-03 12:10:29 +0200

    Liten Xp øvelse

---
+++ Kartlegging.Test/KartleggingsTest.cs
@@ -0,0 +1,22 @@
+﻿using Kartleggingstest;
+
+namespace Kartlegging.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void LvlOne()
+    {
+        var lvl = new LvL();
+        var addXp = lvl.AddOneXp();
+        Assert.Equal(1, addXp);
+    }
+
+    [Fact]
+    public void LvlTwo()
+    {
+        var lvl = new LvL();
+        var addLvl = lvl.AddLvl();
+        Assert.True(addLvl >= 10);
+    }
+}
---
+++ Kartlegging/Kartlegging.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+
+namespace Kartleggingstest;
+
+
+public class LvL
+{
+    public int AddOneXp()
+    {
+        return 1;
+    }
+
+    public int AddLvl()
+    {
+        return 10;
+    }
+}
